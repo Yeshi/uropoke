3a56f51e711f9303ec538e28245879ea07b8604c
Yeshi <yoshikema@gmail.com>
2020-01-12 21:07:17 +0900

    router導入ことはじめ

--- public/uropoke/app.js
+++ public/uropoke/app.js
@@ -1,9 +1,25 @@
+const detailElm = { template: "<div>test</div>" };
+const routers = {
+  path: "/detail",
+  component: detailElm
+};
+
+const router = new VueRouter({
+  routers
+});
+
 var app = new Vue({
+  router,
   el: "#result",
   data: {
-    items: null,
+    items: null
   },
   mounted() {
     axios.get("drawList.json").then(response => (this.items = response.data));
   },
+  methods: {
+    showWindow: function(e) {
+      console.log(e, this);
+    }
+  }
 });
