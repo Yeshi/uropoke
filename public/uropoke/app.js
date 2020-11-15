eb39c997bfcd20c3f41ee2fcf738a1f0d6a8f676
Yeshi <yoshikema@gmail.com>
2020-11-16 02:06:22 +0900

    今日のお題機能をとりあえずつける

--- public/uropoke/app.js
+++ public/uropoke/app.js
@@ -1,8 +1,8 @@
 const app = new Vue({
   el: "#result",
   data: {
-		items: {},
-		isActive: undefined,
+    items: {},
+    isActive: undefined,
   },
   mounted() {
     axios.get("drawList.json").then((response) => (this.items = response.data));
@@ -20,12 +20,31 @@ const app = new Vue({
       }
       return count;
     },
-    total: function(){return this.items.length}
+    total: function () { return this.items.length },
+    todayItem: function(){return this.yetList()}
   },
   methods: {
     clickobj: function (index) {
-			console.log(this.isActive, index);
-			this.isActive = index;
+      console.log(this.isActive, index);
+      this.isActive = index;
     },
+    yetList: function () {
+      let yetList = [];
+      let count = 0;
+      let nowDate = new Date();
+      let dateStr = "";
+      dateStr = Number(String(nowDate.getFullYear()) + String(nowDate.getMonth() + 1) + String(nowDate.getDate()));
+
+      for (let i = 0; i < this.items.length; i++) {
+        if (!this.items[i].drawings.length) {
+          yetList[count] = this.items[i].pokeName;
+          count++
+        }
+      }
+      let chance = new Chance(dateStr);
+      let todayItem = chance.natural({min:0,max:yetList.length});
+      console.log(yetList,dateStr,todayItem,yetList[todayItem]);
+      return todayItem,yetList[todayItem];
+    }
   },
 });
